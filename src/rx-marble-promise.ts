8f6925d7c6307af086a6be4b519c87dca3336a7d
Mathias Remshardt <mathias.remshardt@logicline.de>
2020-01-18 10:54:33 +0100

    * adds rxMarble test in promise version

---
+++ src/rx-marble-promise.ts
@@ -0,0 +1,9 @@
+import { Observable } from "rxjs";
+import { mergeMap } from "rxjs/internal/operators";
+
+const multiplyByTwoAsync = (number: number): Promise<number> =>
+  Promise.resolve(number * 2);
+
+export const rxWithPromise = (
+  source$: Observable<number>
+): Observable<number> => source$.pipe(mergeMap(multiplyByTwoAsync));
